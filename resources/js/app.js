698f81e8fb730a89035c56c950b0473850df2af5
Ahmad-Dorri <ahmaddorri110@gmail.com>
2024-06-25 09:44:04 +0330

    Fix the initialization

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,5 @@
 import './bootstrap';
+import {helloByUser, helloByAdmin} from './modules/hello.js';
+
+window.helloByAdmin = helloByAdmin;
+window.helloByUser = helloByUser;
---
+++ resources/js/modules/hello.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function helloByUser(name) {
+  console.log(`Hello, ${name}`);
+}
+
+function helloByAdmin() {
+  console.log(`Hello, welcome to our site`);
+}
+
+export {helloByUser, helloByAdmin};
