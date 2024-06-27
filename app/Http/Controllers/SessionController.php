032bb8ae73f7011f75fa556a0118651149c0ca85
Ahmad-Dorri <ahmaddorri110@gmail.com>
2024-06-27 14:17:45 +0330

    Add ratelimiter to login action

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Validation\ValidationException;
 use Illuminate\View\View;
 
@@ -17,13 +18,24 @@ class SessionController extends Controller
 
     public function store(Request $request): RedirectResponse
     {
+        // rate limit
+
+        if (RateLimiter::tooManyAttempts('login', $perMinute = 1)) {
+            $seconds = RateLimiter::availableIn('login');
+            throw ValidationException::withMessages([
+                'email' => 'Rate limit. You can try again in '.$seconds.' seconds'
+            ]);
+        }
+
+        RateLimiter::increment('login');
         // validate
         $attributes = $request->validate([
             'email' => ['required', 'email', 'max:254'],
             'password' => ['required']
         ]);
+
         // check if the user exits and validate
-        if(! Auth::attempt($attributes)) {
+        if (! Auth::attempt($attributes)) {
             throw ValidationException::withMessages([
                 'email' => 'The credentials doesnt match'
             ]);
